33a35a061144b011dcc54395dd11d9fe8b64f17e
Max Zheng <zz5ra@virginia.edu>
2017-06-19 13:44:02 -0400

    This is second commit

--- src/PrimeNumber.java
+++ src/PrimeNumber.java
@@ -7,10 +7,10 @@ public class PrimeNumber {
 		do {
 			Scanner input = new Scanner(System.in);
 			System.out.print("Enter an integer: ");
-			long number = input.nextInt();
-			
+			long number = input.nextLong();
+
 			boolean numberIsPrime = checkIfNumberIsPrime (number);
-			
+
 			System.out.print("The Number " + number + " is ");
 			if(numberIsPrime) System.out.println("prime!");
 			else System.out.println("not prime!");
@@ -18,18 +18,19 @@ public class PrimeNumber {
 			again = input.nextInt();
 
 		} while(again == 1);
-		
-		
+
+
 	}
-	
+
 	private static boolean checkIfNumberIsPrime(long number) {
 		if(number == 1 || number == 0) {
 			return false;
 		}
 		boolean isPrime = true;
-		for(int i = 2; i <= (int)Math.sqrt(number); i++) {
+		for(int i = 2; i <= (long)Math.sqrt(number); i++) {
 			if(number % i == 0 && number != i) {
 				isPrime = false;
+				break;
 			}
 		}
 		return isPrime;
