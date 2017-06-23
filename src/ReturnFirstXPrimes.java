f8ca705d4d89d66c4b086d7168c4ad0daf434a3d
max-zheng <zz5ra@virginia.edu>
2017-06-23 15:17:34 -0400

    Add formatting to display and check for condition where number of primes is less than 1

--- src/ReturnFirstXPrimes.java
+++ src/ReturnFirstXPrimes.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ReturnFirstXPrimes {
@@ -7,7 +8,12 @@ public class ReturnFirstXPrimes {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the number of primes you'd like to calculate: ");
         int numberOfPrimes = input.nextInt();
-        ArrayList<Long> primes = new ArrayList<Long>();
+
+        while(numberOfPrimes <= 0) {
+            System.out.println("Please enter a positive number.");
+            numberOfPrimes = input.nextInt();
+        }
+        List<Long> primes = new ArrayList<Long>();
         long primeNumber = 2;
         primes.add(primeNumber);
         while (primes.size() < numberOfPrimes) {
@@ -15,7 +21,7 @@ public class ReturnFirstXPrimes {
             primes.add(nextPrime);
             primeNumber = nextPrime;
         }
-        System.out.println(primes);
+        System.out.println(buildPrimeDisplay(primes));
     }
 
     protected static Long findNextPrime(long previousPrime) {
@@ -26,5 +32,18 @@ public class ReturnFirstXPrimes {
                 return number;
             }
         }
+    }
+
+    private static String buildPrimeDisplay( List<Long> primes) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("#1:2, ");
+        for(int i = 1; i < primes.size(); i++) {
+            builder.append("#" + (i + 1) + ":" + primes.get(i) + ", ");
+            if(i % 50 == 0) {
+                builder.append("\n");
+            }
+        }
+        builder.delete(builder.length() - 2, builder.length());
+        return builder.toString();
     }
 }
